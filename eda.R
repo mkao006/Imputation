c7efb184e8f0f811a7f4bf1a0ca7e531ac7de82e
mkao006 <mkao006@gmail.com>
2013-04-20 18:23:19 +0200

    Changed the data process

--- eda.R
+++ eda.R
@@ -63,15 +63,6 @@ rm(comGrp.df)
 setnames(comGrp.dt, old = colnames(comGrp.dt),
          new = c("itemCode", "comGroup", "includeGroup"))
 
-## Extract the production commodity group from the FAOSTAT 2 website.
-doc = htmlParse("http://faostat.fao.org/site/384/default.aspx")
-test = getNodeSet(doc, path = "//table")
-prodTable.df = readHTMLTable(test[[10]],
-  header = c("Group Name", "Item FAO Code", "Item HS+ Code", "Item Name",
-    "Definition"), stringsAsFactors = FALSE, skip.rows = 1)
-tmp = prodTable.df[, c("Group Name", "Item FAO Code", "Item Name")]
-
-
 
 ## Read and process the test data
 ## ---------------------------------------------------------------------
@@ -83,30 +74,131 @@ setnames(raw.dt, old = c("ItemGroup", "AREA", "ITEM", "ELE"),
 ## Remove test area and excluded countries 
 raw.dt = subset(raw.dt, !(FAOST_CODE %in% c(351, 296, 357, 76, 245, 246, 246, 43,
     298)))
+FAOcountryProfile[FAOcountryProfile$FAOST_CODE %in%
+                  c(351, 296, 357, 76, 245, 246, 246, 43, 298), "LAB_NAME"]
+
+## Not sure what is column is used, and it is a mixture of numeric and strings.
 raw.dt$itemGroup = NULL
 
+
+## Manipulate the data before processing
 mraw.df = melt(raw.dt, c("FAOST_CODE", "itemCode", "elementCode"))
 mraw.df$type = ifelse(grepl("F", mraw.df$variable), "symb", "value")
-
-
 mraw.df$variable = as.numeric(gsub("[^0-9]", "", mraw.df$variable))
 cmraw.df = dcast(mraw.df, FAOST_CODE + itemCode + variable ~ type + elementCode)
-colnames(cmraw.df) = c("FAOST_CODE", "itemCode", "year",
+colnames(cmraw.df) = c("FAOST_CODE", "itemCode", "Year",
         "symbArea", "symbProd", "valueArea", "valueProd")
 cmraw.df$valueProd = as.numeric(cmraw.df$valueProd)
 cmraw.df$valueArea = as.numeric(cmraw.df$valueArea)
 cmraw.df$symbArea = gsub("[[:space:]]", "", cmraw.df$symbArea)
 cmraw.df$symbProd = gsub("[[:space:]]", "", cmraw.df$symbProd)
-cmraw.df$valueYield = with(cmraw.df, valueProd/valueArea)
 cmraw.df$symbYield = with(cmraw.df, ifelse(symbArea == "" & symbProd == "",
     "Y", "N"))
 cmraw.df[is.na(cmraw.df$symbYield), "symbYield"] = "N"
 
-final.df = merge(cmraw.df, FAOcountryProfile[, c("FAOST_CODE", "LAB_NAME")],
+
+## Create the final data frame with country and regional codes and
+## names and also the commodity group.
+process.df = merge(cmraw.df, comGrp.dt, all.x = TRUE)
+
+## These items does not have commodity group from the file given by Hans.
+sort(unique(process.df[is.na(process.df$comGroup), "itemCode"]))
+unique(FAOmetaTable$itemTable[FAOmetaTable$itemTable$itemCode %in%
+                              sort(unique(process.df[is.na(process.df$comGroup),
+                                                     "itemCode"])),
+                              c("itemCode", "itemName")])
+
+process.df = process.df[!is.na(process.df$comGroup), ]
+
+    
+## Merge with regional and subregional
+process.df = merge(process.df, FAOcountryProfile[, c("FAOST_CODE", "LAB_NAME")],
   all.x = TRUE)
-final.df = arrange(merge(final.df,
-  FAOregionProfile[, c("FAOST_CODE", "UNSD_SUB_REG",
-  "UNSD_MACRO_REG")], all.x = TRUE), FAOST_CODE, itemCode, year)
+process.df = arrange(merge(process.df,
+  FAOregionProfile[, c("FAOST_CODE", "UNSD_SUB_REG_CODE",
+  "UNSD_MACRO_REG_CODE")], all.x = TRUE), FAOST_CODE, itemCode, Year)
+## This is a hack for the region until Fillipo fixes the countryprofile.
+process.df = merge(process.df,
+  na.omit(unique(FAOregionProfile[, c("UNSD_MACRO_REG_CODE", "UNSD_MACRO_REG")])),
+  all.x = TRUE)
+process.df = merge(process.df,
+  na.omit(unique(FAOregionProfile[, c("UNSD_SUB_REG_CODE", "UNSD_SUB_REG")])),
+  all.x = TRUE)
+process.df$UNSD_SUB_REG_CODE = NULL
+process.df$UNSD_MACRO_REG_CODE = NULL
+
+## These countries does not have region or subregion available.
+FAOcountryProfile[FAOcountryProfile$FAOST_CODE %in%
+                  unique(process.df[is.na(process.df$UNSD_MACRO_REG),
+                                    "FAOST_CODE"]), "LAB_NAME"]
+
+FAOcountryProfile[FAOcountryProfile$FAOST_CODE %in%
+                  unique(process.df[is.na(process.df$UNSD_SUB_REG),
+                                    "FAOST_CODE"]), "LAB_NAME"]
+
+
+process.df = process.df[!is.na(process.df$UNSD_MACRO_REG) &
+  !is.na(process.df$UNSD_SUB_REG),]
+
+## Save the original value
+process.df$ovalueArea = process.df$valueArea
+process.df$ovalueProd = process.df$valueProd
+
+## Replace values with symbol T or duplicated F with NA
+process.df[which(process.df$symbArea == "T"), "valueArea"] = NA
+process.df[which(process.df$symbProd == "T"), "valueProd"] = NA
+process.df[which(duplicated(process.df[, c("FAOST_CODE", "itemCode", "symbArea",
+                                 "valueArea")]) & process.df$symbArea == "F"),
+         "valueArea"] = NA
+process.df[which(duplicated(process.df[, c("FAOST_CODE", "itemCode", "symbProd",
+                                 "valueProd")]) & process.df$symbProd == "F"),
+         "valueProd"] = NA
+
+
+## A function to compute yield to account for zeros in area and
+## production. To account fo identification problem, we compute yield
+## as NA if one of area or production is zero or NA.
+computeYield = function(production, area){
+  if(length(production) != length(area))
+    stop("Length of prodduction is not the same as area")
+  yield = ifelse(production != 0 & area != 0, production/area, NA)
+}
+
+process.df$valueYield = with(process.df, computeYield(valueProd, valueArea))
+process.df$ovalueYield = with(process.df, computeYield(ovalueProd, ovalueArea))
+
+## Removing blocks which contains only NA's or zero. 
+process.dt = data.table(process.df)
+tmp = process.dt[, sum(valueArea, valueProd, na.rm = TRUE),
+  by = c("FAOST_CODE", "itemCode")]
+tmp[, allMiss := V1 == 0 | is.na(V1)]
+tmp$V1 = NULL
+rmNA.dt = merge(process.dt, tmp, by = c("FAOST_CODE", "itemCode"))
+
+## Fix conflicting error, it is impossible for harvested area to be
+## zero when production is non-zero and vice versa. The solution is to
+## replace the zero for these entries with NA and try to impute them.
+rmNA.dt[valueArea == 0 & valueProd != 0, valueArea := as.numeric(NA)]
+rmNA.dt[valueArea != 0 & valueProd == 0, valueProd := as.numeric(NA)]
+
+
+## These item are removed if we specify FALSE for useNAAreaGrp, this
+## is because they don't have area data on area.
+rmNA.dt = rmNA.dt[(!comGroup %in% c("milk", "poultry_meat", "othr_meat"))]
+
+## Function to check the sparsity of the data
+checkSparsity = function(Data){
+  image(data.matrix(is.na(Data)))
+  text(rep(0.5, NCOL(Data)), seq(0, 1, length = NCOL(Data)),
+       labels = paste(colnames(Data), " (", round(sapply(X = Data,
+         FUN = function(x) 100 * sum(is.na(x))/length(x)), 2), "%)", sep = ""))
+}
+
+## TODO (Michael): Set the keys for the table
+## final.dt = data.table(rmNA.df[which(rmNA.df$includeGroup & rmNA.df$allMiss), ])
+final.dt = rmNA.dt[c(!allMiss & includeGroup), ]
+final.dt[, includeGroup := NULL]
+final.dt[, allMiss := NULL]
 
 
 diffv = function(x){
@@ -120,42 +212,102 @@ diffv = function(x){
     tmp
 }
 
-
-final.dt = data.table(final.df)
-## final.df$dvalueArea = c(NA, diff(log(final.df$valueArea)))
-## final.dt[, dvalueArea := c(NA, diff(log(valueArea)))]
 final.dt[, dvalueArea := c(NA, diffv(valueArea))]
 final.dt[is.nan(dvalueArea), dvalueArea := NA]
-final.dt[year == 2000, dvalueArea := NA]
-## final.df$dvalueProd = c(NA, diff(log(final.df$valueProd)))
-## final.dt[, dvalueProd := c(NA, diff(log(valueProd)))]
+final.dt[Year == 2000, dvalueArea := NA]
 final.dt[, dvalueProd := c(NA, diffv(valueProd))]
 final.dt[is.nan(dvalueProd), dvalueProd := NA]
-final.dt[year == 2000, dvalueProd := NA]
-## final.df$dvalueYield = c(NA, diff(log(final.df$valueYield)))
-## final.dt[, dvalueYield := c(NA, diff(log(valueYield)))]
+final.dt[Year == 2000, dvalueProd := NA]
 final.dt[, dvalueYield := c(NA, diffv(valueYield))]
 final.dt[is.nan(dvalueYield), dvalueYield := NA]
-final.dt[year == 2000, dvalueYield := NA]
+final.dt[Year == 2000, dvalueYield := NA]
 
 
-final.dt[, varDvalueArea := var(dvalueArea, na.rm = TRUE),
-         by = c("FAOST_CODE", "itemCode")]
-final.dt[, varDvalueProd := var(dvalueProd, na.rm = TRUE),
-         by = c("FAOST_CODE", "itemCode")]
-final.dt[, varDvalueYield := var(dvalueYield, na.rm = TRUE),
-         by = c("FAOST_CODE", "itemCode")]
-final.dt[, meanProd := mean(valueProd, na.rm = TRUE),
-         by = c("FAOST_CODE", "itemCode")]
 
 
-test = unique(subset(final.dt, meanProd >= 300,
-  select = c("varDvalueArea", "varDvalueProd",
-                                 "varDvalueYield")))
-with(test, plot(varDvalueArea, varDvalueYield))
+## ## Read and process the test data
+## ## ---------------------------------------------------------------------
+## raw.dt = data.table(read.csv("nicola_ws_request_12yrs.csv", header = TRUE,
+##     stringsAsFactors = FALSE))
+## setnames(raw.dt, old = c("ItemGroup", "AREA", "ITEM", "ELE"),
+##          new = c("itemGroup", "FAOST_CODE", "itemCode", "elementCode"))
 
-with(final.dt, plot(dvalueArea, dvalueYield, col = rgb(0, 0, 0, alpha = 0.1),
-                    pch = 19))
+## ## Remove test area and excluded countries 
+## raw.dt = subset(raw.dt, !(FAOST_CODE %in% c(351, 296, 357, 76, 245, 246, 246, 43,
+##     298)))
+## raw.dt$itemGroup = NULL
+
+## mraw.df = melt(raw.dt, c("FAOST_CODE", "itemCode", "elementCode"))
+## mraw.df$type = ifelse(grepl("F", mraw.df$variable), "symb", "value")
+
+
+## mraw.df$variable = as.numeric(gsub("[^0-9]", "", mraw.df$variable))
+## cmraw.df = dcast(mraw.df, FAOST_CODE + itemCode + variable ~ type + elementCode)
+## colnames(cmraw.df) = c("FAOST_CODE", "itemCode", "year",
+##         "symbArea", "symbProd", "valueArea", "valueProd")
+## cmraw.df$valueProd = as.numeric(cmraw.df$valueProd)
+## cmraw.df$valueArea = as.numeric(cmraw.df$valueArea)
+## cmraw.df$symbArea = gsub("[[:space:]]", "", cmraw.df$symbArea)
+## cmraw.df$symbProd = gsub("[[:space:]]", "", cmraw.df$symbProd)
+## cmraw.df$valueYield = with(cmraw.df, valueProd/valueArea)
+## cmraw.df$symbYield = with(cmraw.df, ifelse(symbArea == "" & symbProd == "",
+##     "Y", "N"))
+## cmraw.df[is.na(cmraw.df$symbYield), "symbYield"] = "N"
+
+## final.df = merge(cmraw.df, FAOcountryProfile[, c("FAOST_CODE", "LAB_NAME")],
+##   all.x = TRUE)
+## final.df = arrange(merge(final.df,
+##   FAOregionProfile[, c("FAOST_CODE", "UNSD_SUB_REG",
+##   "UNSD_MACRO_REG")], all.x = TRUE), FAOST_CODE, itemCode, year)
+
+
+## diffv = function(x){
+##     T = length(x)
+##     ## Use linear interpolation for weight (this might not be suitable)
+##     x[x == 0] = NA
+##     if(sum(!is.na(x)) >= 2)
+##         ## TODO (Michael): test whether approx or na.approx is faster.
+##         x = na.approx(x)
+##     tmp = c(x[2:T]/x[1:(T - 1)])
+##     tmp
+## }
+
+
+## final.dt = data.table(final.df)
+## ## final.df$dvalueArea = c(NA, diff(log(final.df$valueArea)))
+## ## final.dt[, dvalueArea := c(NA, diff(log(valueArea)))]
+## final.dt[, dvalueArea := c(NA, diffv(valueArea))]
+## final.dt[is.nan(dvalueArea), dvalueArea := NA]
+## final.dt[year == 2000, dvalueArea := NA]
+## ## final.df$dvalueProd = c(NA, diff(log(final.df$valueProd)))
+## ## final.dt[, dvalueProd := c(NA, diff(log(valueProd)))]
+## final.dt[, dvalueProd := c(NA, diffv(valueProd))]
+## final.dt[is.nan(dvalueProd), dvalueProd := NA]
+## final.dt[year == 2000, dvalueProd := NA]
+## ## final.df$dvalueYield = c(NA, diff(log(final.df$valueYield)))
+## ## final.dt[, dvalueYield := c(NA, diff(log(valueYield)))]
+## final.dt[, dvalueYield := c(NA, diffv(valueYield))]
+## final.dt[is.nan(dvalueYield), dvalueYield := NA]
+## final.dt[year == 2000, dvalueYield := NA]
+
+
+## final.dt[, varDvalueArea := var(dvalueArea, na.rm = TRUE),
+##          by = c("FAOST_CODE", "itemCode")]
+## final.dt[, varDvalueProd := var(dvalueProd, na.rm = TRUE),
+##          by = c("FAOST_CODE", "itemCode")]
+## final.dt[, varDvalueYield := var(dvalueYield, na.rm = TRUE),
+##          by = c("FAOST_CODE", "itemCode")]
+## final.dt[, meanProd := mean(valueProd, na.rm = TRUE),
+##          by = c("FAOST_CODE", "itemCode")]
+
+
+## test = unique(subset(final.dt, meanProd >= 300,
+##   select = c("varDvalueArea", "varDvalueProd",
+##                                  "varDvalueYield")))
+## with(test, plot(varDvalueArea, varDvalueYield))
+
+## with(final.dt, plot(dvalueArea, dvalueYield, col = rgb(0, 0, 0, alpha = 0.1),
+##                     pch = 19))
 
 ## The plot suggests that the log-normal distribution may not be a
 ## good validation distribution since many of the fit does not appear
@@ -163,17 +315,17 @@ with(final.dt, plot(dvalueArea, dvalueYield, col = rgb(0, 0, 0, alpha = 0.1),
 pdf(file = "yieldCheck.pdf")
 par(mfrow = c(2, 1))
 for(i in unique(final.dt$itemCode)){
-  yieldRange = range(final.dt[year %in% c(2000, 2010) & itemCode == i &
+  yieldRange = range(final.dt[Year %in% c(2000, 2010) & itemCode == i &
     symbYield == "Y", valueYield], na.rm = TRUE)
-  try({hist(final.dt[year == 2000 & itemCode == i & symbYield == "Y", valueYield],
+  try({hist(final.dt[Year == 2000 & itemCode == i & symbYield == "Y", valueYield],
             breaks = 50, freq = FALSE, xlim = yieldRange)
-  param = fitdistr(na.omit(final.dt[year == 2000 & itemCode == i, valueYield]),
+  param = fitdistr(na.omit(final.dt[Year == 2000 & itemCode == i, valueYield]),
     densfun = "lognormal")
   curve(dlnorm(x, meanlog = param$estimate[1], sdlog = param$estimate[2]),
         add = TRUE, col = "red")})  
-  try({hist(final.dt[year == 2010 & itemCode == i & symbYield == "Y", valueYield],
+  try({hist(final.dt[Year == 2010 & itemCode == i & symbYield == "Y", valueYield],
             breaks = 50, freq = FALSE, xlim = yieldRange)
-  param = fitdistr(na.omit(final.dt[year == 2010 & itemCode == i, valueYield]),
+  param = fitdistr(na.omit(final.dt[Year == 2010 & itemCode == i, valueYield]),
     densfun = "lognormal")
   curve(dlnorm(x, meanlog = param$estimate[1], sdlog = param$estimate[2]),
         add = TRUE, col = "red")})
@@ -241,7 +393,7 @@ par(mfrow = c(3, 1))
 try({
   hist(final.dt[!is.na(dvalueArea) & (abs(dvalueArea) != Inf &
                      itemCode == i & symbArea == ""), dvalueArea],
-            breaks = 300, xlim = c(-10, 10), freq = FALSE,
+            breaks = 500, xlim = c(-10, 20), freq = FALSE,
             xlab = "Distribution of change in area",
        main = paste(unique(FAOmetaTable$itemTable[FAOmetaTable$itemTable$itemCode
               == i, "itemName"]), "(",
@@ -260,16 +412,19 @@ try({
     na.rm = TRUE)
   abline(v = c(pos[1] - (pos[2] - pos[1]), pos[3] + (pos[3] - pos[2])),
          lty = 2)
-  m = mean(final.dt[!is.na(dvalueArea) & (abs(dvalueArea) != Inf &
-     itemCode == i & symbArea == ""), dvalueArea], na.rm = TRUE)
-  sd = sqrt(var(final.dt[!is.na(dvalueArea) & (abs(dvalueArea) != Inf &
-     itemCode == i & symbArea == ""), dvalueArea], na.rm = TRUE))
-  abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)
-  legend("topleft", legend = c("Cauchy", "Quantile", "Std"), lty = 1:3,
+  ## m = mean(final.dt[!is.na(dvalueArea) & (abs(dvalueArea) != Inf &
+  ##    itemCode == i & symbArea == ""), dvalueArea], na.rm = TRUE)
+  ## sd = sqrt(var(final.dt[!is.na(dvalueArea) & (abs(dvalueArea) != Inf &
+  ##    itemCode == i & symbArea == ""), dvalueArea], na.rm = TRUE))
+  ## abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)
+  ## legend("topleft", legend = c("Cauchy", "Quantile", "Std"), lty = 1:3,
+  ##        bty = "n")
+  abline(v = c(0.6, 1.4), lty = 3)
+  legend("topleft", legend = c("Cauchy", "Quantile", "Old"), lty = 1:3,
          bty = "n")
   hist(final.dt[!is.na(dvalueProd) & (abs(dvalueProd) != Inf &
                      itemCode == i & symbProd == ""), dvalueProd],
-            breaks = 300, xlim = c(-10, 10), freq = FALSE,
+            breaks = 500, xlim = c(-10, 20), freq = FALSE,
             xlab = "Distribution of change in Production", main = NULL);
        areaDist = fitdistr(final.dt[!is.na(dvalueProd) &
          (abs(dvalueProd) != Inf & itemCode == i & symbProd == ""), dvalueProd],
@@ -284,14 +439,15 @@ try({
     na.rm = TRUE)
   abline(v = c(pos[1] - (pos[2] - pos[1]), pos[3] + (pos[3] - pos[2])),
          lty = 2)
-  m = mean(final.dt[!is.na(dvalueProd) & (abs(dvalueProd) != Inf &
-     itemCode == i & symbProd == ""), dvalueProd], na.rm = TRUE)
-  sd = sqrt(var(final.dt[!is.na(dvalueProd) & (abs(dvalueProd) != Inf &
-     itemCode == i & symbProd == ""), dvalueProd], na.rm = TRUE))
-  abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)  
+  ## m = mean(final.dt[!is.na(dvalueProd) & (abs(dvalueProd) != Inf &
+  ##    itemCode == i & symbProd == ""), dvalueProd], na.rm = TRUE)
+  ## sd = sqrt(var(final.dt[!is.na(dvalueProd) & (abs(dvalueProd) != Inf &
+  ##    itemCode == i & symbProd == ""), dvalueProd], na.rm = TRUE))
+  ## abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)
+  abline(v = c(0.6, 1.4), lty = 3)  
     hist(final.dt[!is.na(dvalueYield) & (abs(dvalueYield) != Inf &
                      itemCode == i & symbYield == "Y"), dvalueYield],
-            breaks = 300, xlim = c(-10, 10), freq = FALSE,
+            breaks = 500, xlim = c(-10, 20), freq = FALSE,
             xlab = "Distribution of change in yield", main = NULL);
        yieldDist = fitdistr(final.dt[!is.na(dvalueYield) &
          (abs(dvalueYield) != Inf & itemCode == i & symbYield == "Y"),
@@ -306,11 +462,12 @@ try({
     na.rm = TRUE)
   abline(v = c(pos[1] - (pos[2] - pos[1]), pos[3] + (pos[3] - pos[2])),
          lty = 2)
-  m = mean(final.dt[!is.na(dvalueYield) & (abs(dvalueYield) != Inf &
-     itemCode == i & symbYield == "Y"), dvalueYield], na.rm = TRUE)
-  sd = sqrt(var(final.dt[!is.na(dvalueYield) & (abs(dvalueYield) != Inf &
-     itemCode == i & symbYield == "Y"), dvalueYield], na.rm = TRUE))
-  abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)
+  ## m = mean(final.dt[!is.na(dvalueYield) & (abs(dvalueYield) != Inf &
+  ##    itemCode == i & symbYield == "Y"), dvalueYield], na.rm = TRUE)
+  ## sd = sqrt(var(final.dt[!is.na(dvalueYield) & (abs(dvalueYield) != Inf &
+  ##    itemCode == i & symbYield == "Y"), dvalueYield], na.rm = TRUE))
+  ## abline(v = c(m - 3 * sd, m + 3 * sd), lty = 3)
+  abline(v = c(0.6, 1.4), lty = 3)
      })
 }
 graphics.off()
