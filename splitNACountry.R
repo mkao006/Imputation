d354133ec88059e026fa363d124f80f0c1b46508
mkao006 <mkao006@gmail.com>
2013-10-21 13:32:56 +0200

    added in roxygen declaration

--- splitNACountry.R
+++ splitNACountry.R
@@ -1,3 +1,12 @@
+##' This function splits the data into two data.frame depending on
+##' whether the country contain any data.
+##'
+##' @param value The column name for the variable of interest.
+##' @param country The column name of the country.
+##' @param data the name of the data
+##'
+##' @export
+
 splitNACountry = function(value, country, data){
     evalText = paste0("missProp := sum(is.na(", value, "))/length(",
         value, ")")
