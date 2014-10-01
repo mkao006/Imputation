12e4227718d19a9dd8d4b1eecec1a150eddb0ed9
mkao006 <mkao006@gmail.com>
2014-10-02 01:05:09 +0200

    First commit of the alpha testing for Nicolas Sakoff

---
+++ tests/alpha_testing/nicolas/imputation_alpha_testing.R
@@ -0,0 +1,222 @@
+## Load the library
+library(faoswsProductionImputation)
+library(faoswsFlag)
+library(faoswsExtra)
+library(data.table)
+library(lattice)
+library(reshape2)
+library(splines)
+library(lme4)
+
+## Get the raw data
+source("process_raw_data.R")
+files = as.list(dir(splitDataDirectory, full.names = TRUE))
+imputedDataDirectory = paste0("imputed_data/", commodityFolder, "/")
+
+## Set the flag table
+swsOldFlagTable = rbind(faoswsFlagTable,
+    data.frame(flagObservationStatus = c("*", "F", "P"),
+               flagObservationWeights = c(0.9, 0.6, 0.7)))
+swsOldFlagTable[swsOldFlagTable$flagObservationStatus == "E",
+                "flagObservationWeights"] = 0.55
+
+## Further process the data
+testData.lst = lapply(files,
+    FUN = function(x){
+        tmp = data.table(read.csv(file = x, stringsAsFactors = FALSE))
+        tmp[, productionValue := as.numeric(productionValue)]
+        tmp[, areaHarvestedValue := as.numeric(areaHarvestedValue)]
+        ## Remove 2013 "T" values as per instruction of Nicolas.
+        tmp[year == 2013 & productionFlag == "T",
+            productionValue := as.numeric(NA)]
+        tmp[year == 2013 & areaHarvestedFlag == "T",
+            areaHarvestedValue := as.numeric(NA)]
+        tmp[productionFlag == " ", productionFlag := ""]
+        tmp[areaHarvestedFlag == " ", areaHarvestedFlag := ""]
+        })
+
+## This is a hack to match the new system
+lapply(testData.lst,
+       FUN = function(x){
+           x[, productionFlag2 := ""]
+           x[, areaHarvestedFlag2 := ""]
+           x[, yieldFlag2 := ""]
+       }
+       )
+
+
+
+## Compute the yield
+lapply(testData.lst,
+       FUN = function(x)
+       computeYield(productionValue = "productionValue",
+                    productionObservationFlag = "productionFlag",
+                    areaHarvestedValue = "areaHarvestedValue",
+                    areaHarvestedObservationFlag =
+                        "areaHarvestedFlag",
+                    yieldValue = "yieldValue",
+                    yieldObservationFlag = "yieldFlag",
+                    yieldMethodFlag = "yieldFlag2",
+                    newMethodFlag = "i",
+                    flagTable = swsOldFlagTable,
+                    data = x)
+       )
+
+                       
+## Impute the data
+imputedData.lst = lapply(testData.lst,
+    FUN = function(x){
+        print(unique(x$itemName))
+        imputeProductionDomain(data = x,
+                               productionValue = "productionValue",
+                               areaHarvestedValue = "areaHarvestedValue",
+                               yieldValue = "yieldValue",
+                               yearValue = "year",
+                               productionObservationFlag =
+                                   "productionFlag",
+                               areaHarvestedObservationFlag =
+                                   "areaHarvestedFlag",
+                               yieldObservationFlag = "yieldFlag",
+                               productionMethodFlag = "productionFlag2",
+                               areaHarvestedMethodFlag =
+                                   "areaHarvestedFlag2",
+                               yieldMethodFlag = "yieldFlag2",
+                               flagTable = swsOldFlagTable,
+                               imputedFlag = "E",
+                               naFlag = "M",
+                               imputationFlag = "I",
+                               newMethodFlag = "",
+                               maxdf = 5,
+                               yieldFormula = yieldValue ~ -1 + (1 + bs(year, df = 2, degree = 1)|areaCode))
+    }
+       )
+
+
+## This is a hack to match the new system
+lapply(imputedData.lst,
+       FUN = function(x){
+           x[, productionFlag2 := NULL]
+           x[, areaHarvestedFlag2 := NULL]
+           x[, yieldFlag2 := NULL]
+       }
+       )
+
+## Function to save the imputation
+saveImputation = function(x){
+    tmp = x[year >= 2009, ]
+    tmp[, areaHarvestedValue := round(areaHarvestedValue)]
+    tmp[, productionValue := round(productionValue)]
+    tmp[, yieldValue := round(yieldValue * 10000)]
+    meltTmp = melt(tmp,
+        id.var = c("areaCode", "areaName", "itemCode",
+            "itemName", "year"))
+    meltTmp[, type := ifelse(grepl("Value",
+                       as.character(variable)), "num", "symb")]
+    meltTmp[, variable := gsub("Value|Flag", "", variable)]
+    dcast(meltTmp, areaCode + areaName + itemCode + itemName +
+          variable ~ year + type,
+          value.var = "value")
+}
+
+
+## Save the imputation results
+if(!file.exists(imputedDataDirectory))
+    dir.create(imputedDataDirectory)
+
+lapply(imputedData.lst,
+       FUN = function(x){
+           write.csv(saveImputation(x),
+                     file =
+                     paste0(imputedDataDirectory,
+                            unique(x$itemName), ".csv"),
+                     row.names = FALSE, na = "")
+       }
+       )
+
+
+
+pdf(file = "allImputed.pdf",  width = 20, height = 12)
+lapply(imputedData.lst,
+       FUN = function(x){
+           print(
+               xyplot(log(productionValue) ~ year|areaName,
+                      data = x,
+                      group = x$productionFlag == "I",
+                      main = unique(x$itemName))
+               )
+           print(
+               xyplot(log(areaHarvestedValue) ~ year|areaName,
+                      data = x,
+                      group = x$areaHarvestedFlag == "I",
+                      main = unique(x$itemName))
+               )
+           print(
+               xyplot(yieldValue ~ year|areaName,
+                      data = x,
+                      group = x$yieldFlag == "I",
+                      main = unique(x$itemName))
+               )                      
+       }
+       )
+graphics.off()
+
+
+## Impute the data
+imputedDataAuto.lst = lapply(testData.lst,
+    FUN = function(x){
+        print(unique(x$itemName))
+        imputeProductionDomain(data = x,
+                               productionValue = "productionValue",
+                               areaHarvestedValue = "areaHarvestedValue",
+                               yieldValue = "yieldValue",
+                               yearValue = "year",
+                               productionObservationFlag =
+                                   "productionFlag",
+                               areaHarvestedObservationFlag =
+                                   "areaHarvestedFlag",
+                               yieldObservationFlag = "yieldFlag",
+                               productionMethodFlag = "productionFlag2",
+                               areaHarvestedMethodFlag =
+                                   "areaHarvestedFlag2",
+                               yieldMethodFlag = "yieldFlag2",
+                               flagTable = swsOldFlagTable,
+                               imputedFlag = "E",
+                               naFlag = "M",
+                               imputationFlag = "I",
+                               newMethodFlag = "",
+                               maxdf = 5)
+    }
+       )
+
+
+pdf(file = "allImputedAuto.pdf",  width = 20, height = 12)
+lapply(imputedDataAuto.lst,
+       FUN = function(x){
+           print(
+               xyplot(log(productionValue) ~ year|areaName,
+                      data = x,
+                      group = x$productionFlag == "I",
+                      main = unique(x$itemName))
+               )
+           print(
+               xyplot(log(areaHarvestedValue) ~ year|areaName,
+                      data = x,
+                      group = x$areaHarvestedFlag == "I",
+                      main = unique(x$itemName))
+               )
+           print(
+               xyplot(yieldValue ~ year|areaName,
+                      data = x,
+                      group = x$yieldFlag == "I",
+                      main = unique(x$itemName))
+               )                      
+       }
+       )
+graphics.off()
+
+
+## check = imputedData.lst[[19]][areaName == "Switzerland", ]
+## v = check[, productionValue]
+## v[check[, productionFlag == "I"]] = NA
+## ensembleImpute(v, plot = TRUE)
+
---
+++ tests/alpha_testing/nicolas/process_raw_data.R
@@ -0,0 +1,117 @@
+## Load the libraries
+library(faoswsExtra)
+library(data.table)
+library(reshape2)
+library(RJDBC)
+library(FAOSTAT)
+
+## Set the commodity group to extract and impute
+commodityFolder = "fruit_primary"
+commodityCode = "1989"
+splitDataDirectory = paste0("./split_data/", commodityFolder, "/")
+
+## Connect to the database
+drv = JDBC(driverClass = "oracle.jdbc.driver.OracleDriver",
+    classPath = "~/ojdbc14.jar")
+conn = dbConnect(drv, "jdbc:oracle:thin:@lprdbwo1:3310:fstp",
+    user = "demo", password = "demo")
+
+## Function to obtain the items within the commodity group
+getCommodityList = function(conn, commodityGroupCode){  
+    query = paste0("SELECT item, long_name_e
+                    FROM item
+                    WHERE item in (
+                    SELECT item_memb
+                    FROM item_agg_grp
+                    WHERE item_grp=", commodityGroupCode, 
+                    ")
+                    ORDER by item")
+    commodityList = dbGetQuery(conn, query)
+    colnames(commodityList) = c("itemCode", "itemName")
+    commodityList
+}
+
+## Function to get the data
+getProductionData = function(conn, itemCode, yearRange){
+    query = paste0("SELECT area, item, ele, ",
+                    paste0(c("num_", "symb_"), rep(yearRange, each = 2),
+                           collapse = ", "),
+                    " FROM tsv_ics_work_yr
+                    WHERE item in (", paste0(itemCode, collapse = ","),")
+                    AND ele in (31, 41, 51)
+                    ORDER BY area, item, ele")
+    production = dbGetQuery(conn, query)
+    meltedProduction =
+        melt(production, id.var = c("AREA", "ITEM", "ELE"))
+    colnames(meltedProduction)[1:3] =
+        c("areaCode", "itemCode", "elementCode")    
+    meltedProduction$year =
+        as.numeric(gsub("[^0-9]", "" , meltedProduction$variable))
+    meltedProduction$Type =
+        tolower(gsub("[0-9|_]", "" , meltedProduction$variable))
+    meltedProduction$variable = NULL
+    finalProduction =
+        dcast(meltedProduction, areaCode + itemCode + elementCode +
+                  year ~ Type, value.var = "value")
+    finalProduction$num = as.numeric(finalProduction$num)
+    data.table(finalProduction)
+}
+
+              
+
+## Function to process the data into the desired format
+dataProcess = function(data){    
+    countryNameTable =
+        data.table(
+            FAOcountryProfile[, c("FAOST_CODE", "FAO_TABLE_NAME")]
+        )
+    setnames(countryNameTable, old = c("FAOST_CODE", "FAO_TABLE_NAME"),
+             new = c("areaCode", "areaName"))
+    itemNameTable =
+        data.table(
+            unique(FAOmetaTable$itemTable[, c("itemCode", "itemName")])
+        )
+    itemNameTable[, itemCode := as.numeric(itemCode)]
+    named_data=
+        merge(merge(data,
+                    itemNameTable, by = "itemCode", all.x = TRUE),
+              countryNameTable, by = "areaCode", all.x = TRUE)
+    tmp = data.table(recast(data = named_data,
+        id.var = c("areaCode", "areaName", "itemCode", "itemName",
+            "elementCode",  "year"),
+        formula = areaCode + areaName + itemCode + itemName + year ~
+        elementCode + variable, measure.var = c("num", "symb")))
+    setnames(tmp,
+             old = colnames(tmp),
+             new = c("areaCode", "areaName", "itemCode", "itemName",
+                 "year", "areaHarvestedValue", "areaHarvestedFlag",
+                 "yieldValue", "yieldFlag",
+                 "productionValue", "productionFlag"))
+    tmp[, areaHarvestedValue := as.numeric(areaHarvestedValue)]
+    tmp[, productionValue := as.numeric(productionValue)]
+    tmp[, yieldValue := as.numeric(yieldValue)/10000]  
+    tmp
+}
+
+
+## Save the raw data into the repository
+if(!file.exists(splitDataDirectory))
+    dir.create(splitDataDirectory)
+
+## commodityList = getCommodityList(conn, commodityCode)
+commodityList = read.csv(file = "fruit_primary_1989_test_set.csv")
+
+lapply(commodityList$itemCode,
+       FUN = function(x){
+           Data =
+               try(getProductionData(conn, itemCode = x,
+                                 yearRange = 1995:2013))
+           if(!inherits(Data, "try-error")){
+               processsedData = dataProcess(Data)
+               write.csv(processsedData,
+                         file = paste0(splitDataDirectory,
+                             unique(processsedData$itemName), ".csv"),
+                         row.names = FALSE, na = "")
+           }
+       }
+       )
