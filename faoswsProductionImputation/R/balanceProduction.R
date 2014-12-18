83a70c9b33b3098b8822f93c7fcce9570052faf6
rockclimber112358 <rockclimber112358@gmail.com>
2014-12-18 14:09:14 -0700

    Prevent coercion warning in balanceProduction

--- faoswsProductionImputation/R/balanceProduction.R
+++ faoswsProductionImputation/R/balanceProduction.R
@@ -27,6 +27,11 @@ balanceProduction = function(columnNames,
         "yValue", "yObsFlag")
     setnames(data, old = origName, new = tmpName)
     
+    # If data$pMetFlag is all NA's, it may be logical.  This could issue a
+    # warning later but it's not really a problem.  To avoid confusion, 
+    # coerce to a character here.
+    if( is( data$pMetFlag, "logical" ) )
+        data$pMetFlag = as.character( data$pMetFlag )
     data[!is.na(aValue) & is.na(pValue) & !is.na(yValue),
          c("pValue", "pObsFlag", "pMetFlag") :=
          list(aValue * yValue,
