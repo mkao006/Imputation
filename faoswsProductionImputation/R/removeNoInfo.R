bdc51fd6bdf51c5eccdfbdd25a0b926ee9f1e31c
rockclimber112358 <rockclimber112358@gmail.com>
2014-12-18 14:43:15 -0700

    removeNoInfo now modifies passed data.table

--- faoswsProductionImputation/R/removeNoInfo.R
+++ faoswsProductionImputation/R/removeNoInfo.R
@@ -9,12 +9,24 @@
 ##' @param value The value of the production.
 ##' @param flag The flag/symbol of production.
 ##' @param byKey The unique keys which identify groups.
+##' @param environment The functions in this package generally work by
+##' passing the data.table by reference and modifying it in place.  In place
+##' row deletion is currently not supported by data.table.  Thus, to make this
+##' function consistent with the other functions in this package, environment
+##' is passed to specify where the modified data.table should be placed.  The
+##' default of parent.frame(1) gives the calling environment and this should be
+##' satisfactory for most, if not all, use cases.
 ##'
 ##' @export
 ##' 
 
-removeNoInfo = function (data, value, flag, byKey){
+removeNoInfo = function (data, value, flag, byKey,
+    environment = parent.frame(1)){
     info = data[, rep(containInfo(get(value), get(flag)),
         NROW(.SD)), by = byKey]$V1
-    data = data[info, ]
+    
+    #Assign the new data.table to environment
+    dataTableName = as.character( match.call()$data )
+    assign( x = dataTableName, value = data[info,],
+        envir = environment )
 }
