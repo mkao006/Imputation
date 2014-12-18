47a24102142a8d25e62320d918abe97f45067135
rockclimber112358 <rockclimber112358@gmail.com>
2014-12-18 14:11:19 -0700

    Revert to old defaultLoess with new span parameter

--- faoswsProductionImputation/R/defaultLoess.R
+++ faoswsProductionImputation/R/defaultLoess.R
@@ -9,38 +9,15 @@ defaultLoess = function(x){
     n.obs = length(na.omit(x))
     if( n.obs < 5)
         return( rep(NA, length.out=T) )
-    ## Need to check this span
-    span = ifelse(n.obs/T >= 0.5, 0.3, ifelse(n.obs >= 10, 0.75, 1))
-    loessFit = tryCatch({
-        fit = predict(loess(formula = x ~ time,
-            control = loess.control(surface = "direct"),
-            span = span, degree = 1),
-            newdata = data.frame(time))
-        list(fit=fit, failure="none")}
-        ,silent = TRUE
-        ,warning = function(w){
-            fit = predict(loess(formula = x ~ time,
-                control = loess.control(surface = "direct"),
-                span = span, degree = 1),
-                newdata = data.frame(time))
-            failure = lapply(w, grepl, pattern="(pseudoinverse |
-                    reciprocal condition number |
-                    neighborhood radius 1 |
-                    other near singularities)" )
-            failure = do.call("any", failure)
-            if(failure)
-                return(list(fit=fit, failure="warning"))
-            else
-                return(list(fit=fit, failure="none"))
-        }
-        ,error = function(e){
-            return( list(fit=rep(NA,length(x)), failure="error") )
-        } )
-    loessFit = switch(loessFit$failure
-           ,error = rep(NA, T)
-           ,warning = loessFixedEquivalentNumberParams(x)
-           ,none = loessFit$fit )
-    loessFit[loessFit<0] = 0
+    span = 4 / sum(!is.na(x))
+    loessFit = try(predict(loess(formula = x ~ time,
+        control = loess.control(surface = "direct"),
+        span = span, degree = 1),
+        newdata = data.frame(time)), silent = TRUE)
+    if(!inherits(loessFit, "try-error") & n.obs >= 5){
+        loessFit[loessFit < 0] = 0
+    } else {
+        loessFit = rep(NA, T)
+    }
     loessFit
 }
-
