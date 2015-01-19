6a5b54f1ed920479a12d424f4ea8094669129ca3
rockclimber112358 <rockclimber112358@gmail.com>
2015-01-19 08:46:13 +0100

    Default median model

---
+++ faoswsProductionImputation/R/defaultMedian.R
@@ -0,0 +1,18 @@
+##' The default median model for the ensemble model.
+##'
+##' @param x A numeric vector to be imputed.
+##' @export
+
+
+defaultMedian = function(x){
+
+    ### Data Quality Checks
+    stopifnot(is.numeric(x))
+    stopifnot(length(x) > 1)
+
+    yearCount = length(x)
+    if(all(is.na(x)))
+        return(as.numeric(rep(NA, yearCount)))
+    medianFit = rep(median(x, na.rm = !all(is.na(x))), yearCount)
+    medianFit
+}
